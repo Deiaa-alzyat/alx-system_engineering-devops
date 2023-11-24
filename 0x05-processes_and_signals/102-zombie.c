cfc5a2dc2ea761e33f9068c0193f592208eb221c
Deiaa-alzyat <yousef.elzyat11@gmail.com>
2023-11-24 10:29:56 -0800

    0x05. Processes and signals

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -4,47 +4,53 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-// Function declaration
+/*Function declaration*/
 int infinite_while(void);
 
 /**
  * main - Creates five zombie processes.
- *
  * Return: Always 0.
  */
 int main(void)
 {
-    pid_t pid;
-    char count = 0;
+	pid_t pid;
+	char count = 0;
 
-    while (count < 5)
-    {
-        pid = fork();
-        if (pid > 0)
-        {
-            printf("Zombie process created, PID: %d\n", pid);
-            sleep(1);
-            count++;
-        }
-        else
-            exit(0);
-    }
+	while (count < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			count++;
+		}
+		else if (pid == 0)
+		{
+			/*Child process*/
+			sleep(1);
+			exit(0);
+		}
+		else
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+	}
 
-    infinite_while();
+	infinite_while();
 
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
 /**
  * infinite_while - Run an infinite while loop.
- *
- * Return: Always 0.
+ * Return: This function never returns.
  */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
