c97d9912232687b1215232f1cbe8d59cdf02f527
Deiaa-alzyat <yousef.alzyat11@gmail.com>
2024-01-19 10:59:18 -0800

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 #!/usr/bin/puppet
 # Install Flask version 2.1.0 using pip3
 package { 'flask':
-  ensure   => '2.1.0',
+  ensure   => '2.1.0',  # Ensures the latest version is installed
+  provider => 'pip3',
+}
+package { 'werkzeug':
+  ensure   => 'latest',  # Ensures the latest version is installed
   provider => 'pip3',
 }
